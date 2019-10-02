90222b916396c4ef20df9bff8c8a33caaacf0dff
Thiago <tjcm@cesar.school>
2019-10-02 08:53:05 -0300

    Ajustes colaborativos

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const SlackBot = require('slackbots');
-const axios = require('axios');
+const SlackBot = require("slackbots");
+const axios = require("axios");
 // const fs = require('fs')
-
 
 const bot = new SlackBot({
   // * lembrar de nao subir o token
-  token: '',  // * pegar o token separado  
-  name: 'cesinhabot'
+  // Token para hackatober
+  token: "", // * pegar o token separado
+  name: "cesinhabot"
 });
 
 // Start Handler
-bot.on('start', () => {
+bot.on("start", () => {
   const params = {
-    icon_emoji: ':robot:'
+    icon_emoji: ":robot:"
   };
 
-  bot.postMessageToChannel('general','Ola! eu sou o bot Cesinha', params);
+  bot.postMessageToChannel("general", "Ola! eu sou o bot Cesinha", params);
 });
 
 // Error Handler
-bot.on('error', err => console.log(err));
+bot.on("error", err => console.log(err));
 
 // cria o modelo de json do horario
 let horario = {
@@ -29,17 +29,14 @@ let horario = {
   Quarta: [],
   Quinta: [],
   Sexta: []
-}
+};
 
 // TODO: arrumar esse erro bizonho
 // Message Handler
 bot.on("message", data => {
-  
   if (data.text.includes("Segunda")) {
-    data.text.replace("Segunda:", "")
-    const aulas = data.text.split(",")
-    horario.Segunda = aulas
+    data.text.replace("Segunda:", "");
+    const aulas = data.text.split(",");
+    horario.Segunda = aulas;
   }
-
-})
-
+});
